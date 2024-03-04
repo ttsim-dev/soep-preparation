5f2cb56f1d4544dfb27efe70ec03bbc826dd11be
felixschmitz <s87fschm@uni-bonn.de>
2024-03-04 22:46:53 +0100

    Finished all cleaning and melting of columns, except birth_month_child_

--- src/data_management_von_gaudecker/data_management/task_clean_datasets.py
+++ src/data_management_von_gaudecker/data_management/task_clean_datasets.py
@@ -1,8 +1,9 @@
 from pytask import task
 
 from data_management_von_gaudecker.config import SRC, BLD
+from data_management_von_gaudecker.data_helper.data_loader import dta_loader
 from data_management_von_gaudecker.data_helper.data_cleaning_parametrization import create_parametrization
-import data_management_von_gaudecker.data_specific_cleaner.clean_biobirth as clean_biobirth
+from data_management_von_gaudecker.data_specific_cleaner.clean_biobirth import clean_biobirth, melt_biobirth
 
 ID_TO_KWARGS = create_parametrization()
 
@@ -12,5 +13,7 @@ for id_, kwargs in ID_TO_KWARGS.items():
     def task_clean_one_dataset(depends_on, produces, data_set_name):
         # TODO: write docstring
         """Clean one dataset."""
-        out = clean_biobirth.clean_biobirth(depends_on)
-        out.to_pickle(produces)
+        raw_data = dta_loader(depends_on)
+        cleaned = clean_biobirth(raw_data)
+        long_cleaned = melt_biobirth(cleaned)
+        long_cleaned.to_pickle(produces)
--- src/data_management_von_gaudecker/data_specific_cleaner/clean_biobirth.py
+++ src/data_management_von_gaudecker/data_specific_cleaner/clean_biobirth.py
@@ -2,24 +2,57 @@ import pandas as pd
 from pathlib import Path
 
 from data_management_von_gaudecker.config import SRC, BLD
-from data_management_von_gaudecker.data_helper.data_loader import dta_loader
 
-def _kid_var_name_mapping() -> dict:
-    """Map the numerical variables."""
-    return {
-        "kidpnr": "p_id_child",
-        "kidgeb": "birth_year_child",
-        "kidmon": "birth_month_child",
-    }
+CATEGORIES_TO_REMOVE = ["[-5] In Fragebogenversion nicht enthalten", "[-3] nicht valide", "[-2] trifft nicht zu", "[-1] keine Angabe"] # move to different place
 
 
-def clean_biobirth(raw_path: Path) -> pd.DataFrame:
+def _remove_irrelevant_categories(sr: pd.Series) -> pd.Series:
+    """Remove irrelevant categories from a series."""
+    removing_categories = list(set(sr.cat.categories) & set(CATEGORIES_TO_REMOVE))
+    return sr.cat.remove_categories(removing_categories)
+
+
+def _integer_category_transformation(sr: pd.Series) -> pd.Series:
+    """Transform a series to an integer."""
+    return sr.cat.rename_categories(sr.cat.categories.astype("Int64"))
+
+def _birth_month_child_categories(sr: pd.Series) -> pd.Series:
+    """Set the categories of the birth month of a child."""
+    return sr.cat.set_categories(
+        [
+            "Januar",
+            "Februar",
+            "Maerz",
+            "April",
+            "Mai",
+            "Juni",
+            "Juli",
+            "August",
+            "September",
+            "Oktober",
+            "November",
+            "Dezember",
+        ]
+    )
+
+
+def clean_biobirth(raw_data: pd.DataFrame) -> pd.DataFrame:
     """Clean the biobirth dataset."""
-    raw_data = dta_loader(raw_path)
     out = pd.DataFrame()
-    out["hh_id_orig"] = raw_data["cid"]
-    for old_name, new_name in _kid_var_name_mapping().items():
-        for id_ in range(1, 16):
-            two_digit_id_ = "{:02}".format(id_)
-            out[f"{new_name}_{two_digit_id_}"] = raw_data[f"{old_name}{two_digit_id_}"]
+    out["hh_id_orig"] = raw_data["cid"].astype("Int64")
+    out["p_id"] = raw_data["pid"].astype("Int64")
+    out["n_kids_total"] = raw_data["sumkids"].astype("Int64")
+    for i in range(1, 16):
+        two_digit = f"{i:02d}"
+        out[f"birth_year_child_{i}"] = _integer_category_transformation(_remove_irrelevant_categories(raw_data[f"kidgeb{two_digit}"])) # convert to int and reset categories ?
+        out[f"p_id_child_{i}"] = _integer_category_transformation(_remove_irrelevant_categories(raw_data[f"kidpnr{two_digit}"])) # convert to int and reset categories ?
+        out[f"birth_month_child_{i}"] = _birth_month_child_categories(_remove_irrelevant_categories(raw_data[f"kidmon{two_digit}"]))
     return out
+
+    # TODO: there is still a bug regarding the cleaning and transformation of birthmonth data
+
+def melt_biobirth(data: pd.DataFrame) -> pd.DataFrame:
+    """Melt the biobirth dataset."""
+    # breakpoint()
+    out = data.melt(id_vars=["hh_id_orig", "p_id", "n_kids_total"])
+    return out.dropna(subset=['value']).reset_index(drop=True)
