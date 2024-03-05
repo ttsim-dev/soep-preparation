c4802d78685406ece86ea010892a996a7497315c
felixschmitz <s87fschm@uni-bonn.de>
2024-03-05 11:06:01 +0100

    Refactor global param of CATEGORIES_TO_REMOVE

--- src/data_management_von_gaudecker/data_management/task_clean_datasets.py
+++ src/data_management_von_gaudecker/data_management/task_clean_datasets.py
@@ -1,9 +1,8 @@
 from pytask import task
 
-from data_management_von_gaudecker.config import SRC, BLD
 from data_management_von_gaudecker.data_helper.data_loader import dta_loader
 from data_management_von_gaudecker.data_helper.data_cleaning_parametrization import create_parametrization
-from data_management_von_gaudecker.data_specific_cleaner.clean_biobirth import clean_biobirth, melt_biobirth
+from data_management_von_gaudecker.data_management.data_specific_cleaner.clean_biobirth import clean_biobirth, melt_biobirth
 
 ID_TO_KWARGS = create_parametrization()
 
@@ -11,8 +10,14 @@ for id_, kwargs in ID_TO_KWARGS.items():
 
     @task(id=id_, kwargs=kwargs)
     def task_clean_one_dataset(depends_on, produces, data_set_name):
-        # TODO: write docstring
-        """Clean one dataset."""
+        """
+        Clean one dataset.
+        
+        Args:
+            depends_on (str): Path to the raw data file.
+            produces (str): Path to save the cleaned data file.
+            data_set_name (str): Name of the dataset.
+        """
         raw_data = dta_loader(depends_on)
         cleaned = clean_biobirth(raw_data)
         long_cleaned = melt_biobirth(cleaned)
