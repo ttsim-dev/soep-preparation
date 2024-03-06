7651a08233559624a5a8895f83a574193921e9fe
felixschmitz <s87fschm@uni-bonn.de>
2024-03-06 11:11:09 +0100

    Addendum removing template example, removing paths from config etc.

--- src/data_management_von_gaudecker/config.py
+++ src/data_management_von_gaudecker/config.py
@@ -7,8 +7,6 @@ BLD = SRC.joinpath("..", "..", "bld").resolve()
 TEST_DIR = SRC.joinpath("..", "..", "tests").resolve()
 PAPER_DIR = SRC.joinpath("..", "..", "paper").resolve()
 
-GROUPS = ["marital_status", "qualification"]
-
 CATEGORIES_TO_REMOVE = ["[-8] Frage in diesem Jahr nicht Teil des Frageprogramms", "[-5] in Fragebogenversion nicht enthalten", "[-3] nicht valide", "[-2] trifft nicht zu", "[-1] keine Angabe"]
 
-__all__ = ["BLD", "SRC", "TEST_DIR", "GROUPS", "CATEGORIES_TO_REMOVE"]
+__all__ = ["BLD", "SRC", "TEST_DIR", "CATEGORIES_TO_REMOVE"]
