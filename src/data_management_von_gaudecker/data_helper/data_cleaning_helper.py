f333eba73d3b21ceb8990d639d678da1c43ec650
felixschmitz <s87fschm@uni-bonn.de>
2024-03-06 10:45:39 +0100

    Generalization and renaming of categorical cleaning functions

--- src/data_management_von_gaudecker/data_helper/data_cleaning_helper.py
+++ src/data_management_von_gaudecker/data_helper/data_cleaning_helper.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 from data_management_von_gaudecker.config import CATEGORIES_TO_REMOVE
 
-
 def _remove_irrelevant_categories(sr: pd.Series) -> pd.Series:
     """Remove irrelevant categories from a series."""
     removing_categories = list(set(sr.cat.categories) & set(CATEGORIES_TO_REMOVE))
@@ -11,9 +10,9 @@ def _remove_irrelevant_categories(sr: pd.Series) -> pd.Series:
 def _integer_category_cleaning(sr: pd.Series) -> pd.Series:
     """Transform a series to an integer."""
     sr = _remove_irrelevant_categories(sr)
-    return sr.cat.rename_categories(sr.cat.categories.astype("Int64"))
+    return sr.cat.rename_categories(sr.cat.categories.astype("Int32"))
 
-def _month_category_cleaning(sr: pd.Series) -> pd.Series:
+def _categorical_string_cleaning(sr: pd.Series) -> pd.Series:
     """Set the categories of the birth month of a child."""
     sr = _remove_irrelevant_categories(sr)
-    return sr.cat.rename_categories([i.split(" ")[1] for i in sr.cat.categories])
+    return sr.cat.rename_categories([i.split(" ", 1)[1] for i in sr.cat.categories])
