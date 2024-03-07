7a10879563d41fb10d0fd2457f559c58eee0c286
felixschmitz <s87fschm@uni-bonn.de>
2024-03-07 13:33:57 +0100

    Wrote function to clean categorical integer columns

--- src/soep_cleaning/data_helper/data_cleaning_helper.py
+++ src/soep_cleaning/data_helper/data_cleaning_helper.py
@@ -1,18 +1,24 @@
 import pandas as pd
 
 from soep_cleaning.config import CATEGORIES_TO_REMOVE
+from soep_cleaning.utilities import find_lowest_int_dtype
 
 def _remove_irrelevant_categories(sr: pd.Series) -> pd.Series:
     """Remove irrelevant categories from a series."""
-    removing_categories = list(set(sr.cat.categories) & set(CATEGORIES_TO_REMOVE))
+    str_categories = sr.cat.categories[sr.cat.categories.map(lambda x: isinstance(x, str))]
+    removing_categories = list(set(str_categories) & CATEGORIES_TO_REMOVE)
     return sr.cat.remove_categories(removing_categories)
 
-def _integer_category_cleaning(sr: pd.Series) -> pd.Series:
-    """Transform a series to an integer."""
-    sr = _remove_irrelevant_categories(sr)
-    return sr.cat.rename_categories(sr.cat.categories.astype("Int32"))
-
 def _categorical_string_cleaning(sr: pd.Series) -> pd.Series:
-    """Set the categories of the birth month of a child."""
+    """Clean categorial categories with preceiding numbering."""
     sr = _remove_irrelevant_categories(sr)
     return sr.cat.rename_categories([i.split(" ", 1)[1] for i in sr.cat.categories])
+
+def _categorical_int_cleaning(sr: pd.Series) -> pd.Series:
+    """Transform a series to an ordered categorical containing integers."""
+    if sr.dtype.name == "category":
+        sr = _remove_irrelevant_categories(sr)
+    sr = sr.astype(find_lowest_int_dtype(sr))
+    categories_order = sr.sort_values().unique().dropna().tolist()
+    sr = sr.astype("category")
+    return sr.cat.set_categories(categories_order, rename=True, ordered=True)
