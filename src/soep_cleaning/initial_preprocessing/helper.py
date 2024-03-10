968cf47bcee33fe75af8095beb0cf02cee372733
felixschmitz <s87fschm@uni-bonn.de>
2024-03-10 16:31:31 +0100

    Error handling for data cleaning helper functions

--- src/soep_cleaning/initial_preprocessing/helper.py
+++ src/soep_cleaning/initial_preprocessing/helper.py
@@ -30,14 +30,14 @@ def _remove_missing_data_categories(sr: pd.Series) -> pd.Series:
 def str_categorical(
     sr: "pd.Series[str]",
     no_identifiers: int = 1,
-    unordered: bool = False,
+    ordered: bool = True,
 ) -> "pd.Series[str]":
     """Clean the categories of a pd.Series of dtype category with str entries.
 
     Parameters:
         sr (pd.Series[str]): The input series with categories to be cleaned.
         no_identifiers (int, optional): The number of identifiers inside each category to be removed. Defaults to 1.
-        unordered (bool, optional): Whether the series should be returned as unordered. Defaults to False.
+        ordered (bool, optional): Whether the series should be returned as unordered. Defaults to True.
 
     Returns:
         pd.Series[str]: The series with cleaned categories.
@@ -59,8 +59,13 @@ def str_categorical(
         Categories (3, object): ['1984 Ausgangs-Sample (West)', '1984 Migration (bis 1983, West)', '1990 Ausgangs-Sample (Ost)']
 
     """
+    _error_handling_categorical(
+        sr,
+        "category",
+        [[sr, "pandas.core.series.Series"], [no_identifiers, "int"], [ordered, "bool"]],
+    )
     sr = _remove_missing_data_categories(sr)
-    if unordered:
+    if not ordered:
         sr = sr.cat.as_unordered()
     return sr.cat.rename_categories(
         [i.split(" ", no_identifiers)[no_identifiers] for i in sr.cat.categories],
@@ -78,6 +83,11 @@ def int_categorical(sr: "pd.Series[int]", ordered: bool = False) -> "pd.Series[i
         pd.Series[int]: The series with cleaned categories.
 
     """
+    _error_handling_categorical(
+        sr,
+        "category",
+        [[sr, "pandas.core.series.Series"], [ordered, "bool"]],
+    )
     sr = _remove_missing_data_categories(sr)
     return int_to_int_categorical(sr, ordered)
 
@@ -96,6 +106,9 @@ def int_to_int_categorical(
         pd.Series[int]: The series with int categories.
 
     """
+    _fail_if_series_wrong_dtype(sr, "int")
+    _fail_if_invalid_input(sr, "pandas.core.series.Series")
+    _fail_if_invalid_input(ordered, "bool")
     sr = apply_lowest_int_dtype(sr)
     categories_order = sr.sort_values().unique().dropna().tolist()
     sr = sr.astype("category")
@@ -114,6 +127,7 @@ def bool_categorical(sr: "pd.Series[str]") -> "pd.Series[bool]":
         pd.Series[int]: The series with cleaned categories.
 
     """
+    _error_handling_categorical(sr, "category", [[sr, "pandas.core.series.Series"]])
     sr = sr.astype("category")
     return sr.cat.set_categories([False, True], rename=True, ordered=True)
 
@@ -128,6 +142,7 @@ def biobirth_wide_to_long(df: pd.DataFrame) -> pd.DataFrame:
         pd.DataFrame: The dataframe in long format.
 
     """
+    _fail_if_invalid_input(df, "pandas.core.frame.DataFrame")
     prev_wide_cols = ["birth_year_child", "p_id_child", "birth_month_child"]
     df = pd.wide_to_long(
         df,
@@ -156,6 +171,7 @@ def hwealth_wide_to_long(df: pd.DataFrame) -> pd.DataFrame:
         pd.DataFrame: The dataframe in long format.
 
     """
+    _fail_if_invalid_input(df, "pandas.core.frame.DataFrame")
     prev_wide_cols = [
         "wohnsitz_immobilienverm_hh",
         "finanzverm_hh",
@@ -203,5 +219,40 @@ def int_categorical_to_int(sr: "pd.Series[category]") -> "pd.Series[int]":
         pd.Series[int]: The series with cleaned categories.
 
     """
+    _error_handling_categorical(sr, "category", [[sr, "pandas.core.series.Series"]])
     sr = _remove_missing_data_categories(sr)
     return apply_lowest_int_dtype(sr)
+
+
+def _error_handling_categorical(
+    sr: pd.Series,
+    expected_sr_dtype: str,
+    input_expected_types: list[list] | None = None,
+):
+    if input_expected_types is None:
+        input_expected_types = [[]]
+    _fail_if_series_wrong_dtype(sr, expected_sr_dtype)
+    _fail_series_without_categories(sr)
+    for item in input_expected_types:
+        inputt, expected_type = item
+        _fail_if_invalid_input(inputt, expected_type)
+
+
+def _fail_if_series_wrong_dtype(sr: pd.Series, expected_dtype: str):
+    if expected_dtype != sr.dtype.name:
+        msg = f"Expected dtype {expected_dtype}, got {sr.dtype.name}"
+        raise TypeError(msg)
+
+
+def _fail_series_without_categories(sr: pd.Series):
+    if sr.cat.categories is None:
+        msg = "Series has no categories"
+        raise ValueError(msg)
+
+
+def _fail_if_invalid_input(inputt, expected_dtype: str):
+    if expected_dtype not in str(type(inputt)):
+        msg = f"Expected {inputt} to be of type {expected_dtype}, got {type(inputt)}"
+        raise TypeError(
+            msg,
+        )
