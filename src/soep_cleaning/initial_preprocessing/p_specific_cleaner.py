ca8b089515b799c3cab51f8156683d76591bad68
felixschmitz <s87fschm@uni-bonn.de>
2024-03-10 18:49:43 +0100

    Clean pequiv dataset.

--- src/soep_cleaning/initial_preprocessing/p_specific_cleaner.py
+++ src/soep_cleaning/initial_preprocessing/p_specific_cleaner.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 from soep_cleaning.initial_preprocessing.helper import (
+    agreement_int_categorical,
+    bool_categorical,
     int_categorical_to_int,
     str_categorical,
 )
@@ -31,91 +33,144 @@ def pbrutto(raw_data: pd.DataFrame) -> pd.DataFrame:
 def pequiv(raw_data: pd.DataFrame) -> pd.DataFrame:
     """Clean the pequiv dataset."""
     out = pd.DataFrame()
-    out["soep_initial_hh_id"] = int_categorical_to_int(raw_data["cid"])
-    out["soep_hh_id"] = raw_data["hid"].astype(apply_lowest_int_dtype(raw_data["hid"]))
-    out["p_id"] = int_categorical_to_int(raw_data["pid"])
-    out["year"] = int_categorical_to_int(raw_data["syear"])
-    out["gender"] = raw_data["d11102ll"]
-    out["age"] = raw_data["d11101"]
-    out["hh_size"] = raw_data["d11106"]
-    out["anz_minderj_hh1"] = raw_data["d11107"]
-    out["annual_work_h_prev"] = raw_data["e11101"]
-    out["employment_status_pequiv"] = raw_data["e11102"]
-    out["employment_level_pequiv"] = raw_data["e11103"]
-    out["einkommen_vor_steuer_hh_prev"] = raw_data["i11101"]
-    out["einkommen_nach_steuer_hh_prev"] = raw_data["i11102"]
-    out["labor_earnings_prev"] = raw_data["i11110"]
-    out["bundesland"] = raw_data["l11101"]
-    out["earnings_job_1_prev"] = raw_data["ijob1"]
-    out["earnings_job_2_prev"] = raw_data["ijob2"]
-    out["self_empl_earnings_prev"] = raw_data["iself"]
-    out["arbeitsl_geld_soep_prev"] = raw_data["iunby"]
-    out["arbeitsl_hilfe_soep_prev"] = raw_data["iunay"]
-    out["unterhaltsgeld_soep_prev"] = raw_data["isuby"]
-    out["übergangsgeld_soep_prev"] = raw_data["ieret"]
-    out["mschaftsgeld_soep_prev"] = raw_data["imaty"]
-    out["student_grants_prev"] = raw_data["istuy"]
-    out["alimony_prev"] = raw_data["ialim"]
-    out["other_transfers_prev"] = raw_data["ielse"]
-    out["christmas_bonus_prev"] = raw_data["ixmas"]
-    out["vacation_bonus_prev"] = raw_data["iholy"]
-    out["profit_share_prev"] = raw_data["igray"]
-    out["other_bonuses_prev"] = raw_data["iothy"]
-    out["gov_ret_pay_prev"] = raw_data["igrv1"]
-    out["gov_ret_pay_widow_prev"] = raw_data["igrv2"]
-    out["rental_income_y_hh_prev"] = raw_data["renty"]
-    out["maintenance_c_hh_prev"] = raw_data["opery"]
-    out["capital_income_hh_prev"] = raw_data["divdy"]
-    out["kindergeld_pequiv_hh_prev"] = raw_data["chspt"]
-    out["wohngeld_soep_hh_prev"] = raw_data["house"]
-    out["pflegegeld_hh_prev"] = raw_data["nursh"]
-    out["soc_assist_hh_prev"] = raw_data["subst"]
-    out["soc_assist_spec_hh_prev"] = raw_data["sphlp"]
-    out["housing_sup_hh_prev"] = raw_data["hsup"]
-    out["soc_miners_pension_prev"] = raw_data["ismp1"]
-    out["civ_serv_pension_prev"] = raw_data["iciv1"]
-    out["warvictim_pension_prev"] = raw_data["iwar1"]
-    out["farmer_pension_prev"] = raw_data["iagr1"]
-    out["stat_accident_insurance_prev"] = raw_data["iguv1"]
-    out["civ_supp_benefits_prev"] = raw_data["ivbl1"]
-    out["company_pen_prev"] = raw_data["icom1"]
-    out["private_pen_prev"] = raw_data["iprv1"]
-    out["other_pen_prev"] = raw_data["ison1"]
-    out["soc_miners_pension_widow_prev"] = raw_data["ismp2"]
-    out["civ_serv_pension_widow_prev"] = raw_data["iciv2"]
-    out["warvictim_pension_widow_prev"] = raw_data["iwar2"]
-    out["farmer_pension_widow_prev"] = raw_data["iagr2"]
-    out["stat_accident_insur_widow_prev"] = raw_data["iguv2"]
-    out["civ_supp_benefits_widow_prev"] = raw_data["ivbl2"]
-    out["company_pen_widow_prev"] = raw_data["icom2"]
-    out["other_pen_widow_prev"] = raw_data["ison2"]
-    out["private_pen_widow_prev"] = raw_data["iprv2"]
-    out["med_pe_krnkhaus"] = raw_data["m11101"]
-    out["med_pe_schlaganf"] = raw_data["m11105"]
-    out["med_pe_bluthdrck"] = raw_data["m11106"]
-    out["med_pe_diabetes"] = raw_data["m11107"]
-    out["med_pe_krebs"] = raw_data["m11108"]
-    out["med_pe_psych"] = raw_data["m11109"]
-    out["med_pe_gelenk"] = raw_data["m11110"]
-    out["med_pe_herzkr"] = raw_data["m11111"]
-    out["med_pe_schw_treppen"] = raw_data["m11113"]
-    out["med_pe_schw_anziehen"] = raw_data["m11115"]
-    out["med_pe_schw_bett"] = raw_data["m11116"]
-    out["med_pe_schw_einkauf"] = raw_data["m11117"]
-    out["med_pe_schw_hausarb"] = raw_data["m11119"]
-    out["med_pe_groesse"] = raw_data["m11122"]
-    out["med_pe_gewicht"] = raw_data["m11123"]
-    out["med_pe_zufrieden"] = raw_data["m11125"]
-    out["med_pe_subj_status"] = raw_data["m11126"]
-    out["soc_assist_eld_hh_prev"] = raw_data["ssold"]
-    out["arbeitsl_geld_2_soep_hh_prev"] = raw_data["alg2"]
-    out["kinderzuschlag_pequiv_hh_prev"] = raw_data["adchb"]
-    out["adv_child_maint_payment_prev"] = raw_data["iachm"]
-    out["childcare_subsidy_hh_prev"] = raw_data["chsub"]
-    out["caregiver_alimony_prev"] = raw_data["ichsu"]
-    out["divorce_alimony_prev"] = raw_data["ispou"]
-    out["riester_prev"] = raw_data["irie1"]
-    out["riester_widow_prev"] = raw_data["irie2"]
+    out["soep_initial_hh_id"] = apply_lowest_int_dtype(raw_data["cid"])
+    out["soep_hh_id"] = apply_lowest_int_dtype(raw_data["hid"])
+    out["p_id"] = apply_lowest_int_dtype(raw_data["pid"])
+    out["year"] = apply_lowest_int_dtype(raw_data["syear"])
+    out["gender"] = bool_categorical(raw_data["d11102ll"])
+    out["age"] = int_categorical_to_int(raw_data["d11101"])
+    out["hh_size"] = apply_lowest_int_dtype(raw_data["d11106"])
+    out["anz_minderj_hh1"] = apply_lowest_int_dtype(raw_data["d11107"])
+    out["annual_work_h_prev"] = int_categorical_to_int(raw_data["e11101"])
+    out["employment_status_pequiv"] = str_categorical(raw_data["e11102"], ordered=False)
+    out["employment_level_pequiv"] = str_categorical(raw_data["e11103"], ordered=False)
+    out["einkommen_vor_steuer_hh_prev"] = int_categorical_to_int(raw_data["i11101"])
+    out["einkommen_nach_steuer_hh_prev"] = int_categorical_to_int(raw_data["i11102"])
+    out["labor_earnings_prev"] = int_categorical_to_int(raw_data["i11110"])
+    out["bundesland"] = str_categorical(raw_data["l11101"], ordered=False)
+    out["earnings_job_1_prev"] = int_categorical_to_int(raw_data["ijob1"])
+    out["earnings_job_2_prev"] = int_categorical_to_int(raw_data["ijob2"])
+    out["self_empl_earnings_prev"] = int_categorical_to_int(raw_data["iself"])
+    out["arbeitsl_geld_soep_prev"] = int_categorical_to_int(raw_data["iunby"])
+    out["arbeitsl_hilfe_soep_prev"] = int_categorical_to_int(raw_data["iunay"])
+    out["unterhaltsgeld_soep_prev"] = int_categorical_to_int(raw_data["isuby"])
+    out["übergangsgeld_soep_prev"] = int_categorical_to_int(raw_data["ieret"])
+    out["mschaftsgeld_soep_prev"] = int_categorical_to_int(raw_data["imaty"])
+    out["student_grants_prev"] = int_categorical_to_int(raw_data["istuy"])
+    out["alimony_prev"] = int_categorical_to_int(raw_data["ialim"])
+    out["other_transfers_prev"] = int_categorical_to_int(raw_data["ielse"])
+    out["christmas_bonus_prev"] = int_categorical_to_int(raw_data["ixmas"])
+    out["vacation_bonus_prev"] = int_categorical_to_int(raw_data["iholy"])
+    out["profit_share_prev"] = int_categorical_to_int(raw_data["igray"])
+    out["other_bonuses_prev"] = int_categorical_to_int(raw_data["iothy"])
+    out["gov_ret_pay_prev"] = int_categorical_to_int(raw_data["igrv1"])
+    out["gov_ret_pay_widow_prev"] = int_categorical_to_int(raw_data["igrv2"])
+    out["rental_income_y_hh_prev"] = int_categorical_to_int(raw_data["renty"])
+    out["maintenance_c_hh_prev"] = int_categorical_to_int(raw_data["opery"])
+    out["capital_income_hh_prev"] = int_categorical_to_int(raw_data["divdy"])
+    out["kindergeld_pequiv_hh_prev"] = int_categorical_to_int(raw_data["chspt"])
+    out["wohngeld_soep_hh_prev"] = int_categorical_to_int(raw_data["house"])
+    out["pflegegeld_hh_prev"] = int_categorical_to_int(raw_data["nursh"])
+    out["soc_assist_hh_prev"] = int_categorical_to_int(raw_data["subst"])
+    out["soc_assist_spec_hh_prev"] = int_categorical_to_int(raw_data["sphlp"])
+    out["housing_sup_hh_prev"] = int_categorical_to_int(raw_data["hsup"])
+    out["soc_miners_pension_prev"] = int_categorical_to_int(raw_data["ismp1"])
+    out["civ_serv_pension_prev"] = int_categorical_to_int(raw_data["iciv1"])
+    out["warvictim_pension_prev"] = int_categorical_to_int(raw_data["iwar1"])
+    out["farmer_pension_prev"] = int_categorical_to_int(raw_data["iagr1"])
+    out["stat_accident_insurance_prev"] = int_categorical_to_int(raw_data["iguv1"])
+    out["civ_supp_benefits_prev"] = int_categorical_to_int(raw_data["ivbl1"])
+    out["company_pen_prev"] = int_categorical_to_int(raw_data["icom1"])
+    out["private_pen_prev"] = int_categorical_to_int(raw_data["iprv1"])
+    out["other_pen_prev"] = int_categorical_to_int(raw_data["ison1"])
+    out["soc_miners_pension_widow_prev"] = int_categorical_to_int(raw_data["ismp2"])
+    out["civ_serv_pension_widow_prev"] = int_categorical_to_int(raw_data["iciv2"])
+    out["warvictim_pension_widow_prev"] = int_categorical_to_int(raw_data["iwar2"])
+    out["farmer_pension_widow_prev"] = int_categorical_to_int(raw_data["iagr2"])
+    out["stat_accident_insur_widow_prev"] = int_categorical_to_int(raw_data["iguv2"])
+    out["civ_supp_benefits_widow_prev"] = int_categorical_to_int(raw_data["ivbl2"])
+    out["company_pen_widow_prev"] = int_categorical_to_int(raw_data["icom2"])
+    out["other_pen_widow_prev"] = int_categorical_to_int(raw_data["ison2"])
+    out["private_pen_widow_prev"] = int_categorical_to_int(raw_data["iprv2"])
+
+    out["med_pe_krnkhaus"] = bool_categorical(
+        raw_data["m11101"],
+        renaming={"Does not apply": False, "Applies": True},
+        ordered=True,
+    )
+    out["med_pe_schlaganf"] = bool_categorical(
+        raw_data["m11105"],
+        renaming={0: False, 1: True},
+        ordered=True,
+    )
+    out["med_pe_bluthdrck"] = bool_categorical(
+        raw_data["m11106"],
+        renaming={0: False, 1: True},
+        ordered=True,
+    )
+    out["med_pe_diabetes"] = bool_categorical(
+        raw_data["m11107"],
+        renaming={0: False, 1: True},
+        ordered=True,
+    )
+    out["med_pe_krebs"] = bool_categorical(
+        raw_data["m11108"],
+        renaming={0: False, 1: True},
+        ordered=True,
+    )
+    out["med_pe_psych"] = bool_categorical(
+        raw_data["m11109"],
+        renaming={0: False, 1: True},
+        ordered=True,
+    )
+    out["med_pe_gelenk"] = bool_categorical(
+        raw_data["m11110"],
+        renaming={0: False, 1: True},
+        ordered=True,
+    )
+    out["med_pe_herzkr"] = bool_categorical(
+        raw_data["m11111"],
+        renaming={0: False, 1: True},
+        ordered=True,
+    )
+    out["med_pe_schw_treppen"] = bool_categorical(
+        raw_data["m11113"],
+        renaming={"Does not apply": False, "Applies": True},
+        ordered=True,
+    )
+    out["med_pe_schw_anziehen"] = bool_categorical(
+        raw_data["m11115"],
+        renaming={"Does not apply": False, "Applies": True},
+        ordered=True,
+    )
+    out["med_pe_schw_bett"] = bool_categorical(
+        raw_data["m11116"],
+        renaming={"Does not apply": False, "Applies": True},
+        ordered=True,
+    )
+    out["med_pe_schw_einkauf"] = bool_categorical(
+        raw_data["m11117"],
+        renaming={"Does not apply": False, "Applies": True},
+        ordered=True,
+    )
+    out["med_pe_schw_hausarb"] = bool_categorical(
+        raw_data["m11119"],
+        renaming={"Does not apply": False, "Applies": True},
+        ordered=True,
+    )
+    out["med_pe_groesse"] = int_categorical_to_int(raw_data["m11122"])
+    out["med_pe_gewicht"] = int_categorical_to_int(raw_data["m11123"])
+    out["med_pe_zufrieden"] = agreement_int_categorical(raw_data["m11125"])
+    out["med_pe_subj_status"] = str_categorical(raw_data["m11126"])
+    out["soc_assist_eld_hh_prev"] = int_categorical_to_int(raw_data["ssold"])
+    out["arbeitsl_geld_2_soep_hh_prev"] = int_categorical_to_int(raw_data["alg2"])
+    out["kinderzuschlag_pequiv_hh_prev"] = int_categorical_to_int(raw_data["adchb"])
+    out["adv_child_maint_payment_prev"] = int_categorical_to_int(raw_data["iachm"])
+    out["childcare_subsidy_hh_prev"] = int_categorical_to_int(raw_data["chsub"])
+    out["caregiver_alimony_prev"] = int_categorical_to_int(raw_data["ichsu"])
+    out["divorce_alimony_prev"] = int_categorical_to_int(raw_data["ispou"])
+    out["riester_prev"] = int_categorical_to_int(raw_data["irie1"])
+    out["riester_widow_prev"] = int_categorical_to_int(raw_data["irie2"])
 
     return out
 
