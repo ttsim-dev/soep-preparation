472bbb0f05b5a8588eebad713d24a5e4f397ca20
felixschmitz <s87fschm@uni-bonn.de>
2024-09-04 11:26:05 +0200

    Helper functions for logical cleaning and creation of (dummy) variables.

---
+++ src/soep_cleaning/create_variables/education_mapping.py
@@ -0,0 +1,24 @@
+isced = {
+    "in school": "primary_and_lower_secondary",
+    "Primary education": "primary_and_lower_secondary",
+    "Lower secondary education": "primary_and_lower_secondary",
+    "Upper secondary education": "upper_secondary",
+    "Post-secondary non-tertiary education": "upper_secondary",
+    "Short-cycle tertiary education": "upper_secondary",
+    "Bachelor s or equivalent level": "tertiary",
+    "Master s or equivalent level": "tertiary",
+    "Doctoral or equivalent level": "tertiary",
+}
+
+casmin = {
+    "(0) in school": "primary_and_lower_secondary",
+    "(1a) inadequately completed": "primary_and_lower_secondary",
+    "(1b) general elementary school": "primary_and_lower_secondary",
+    "(1c) basic vocational qualification": "primary_and_lower_secondary",
+    "(2a) intermediate vocational": "upper_secondary",
+    "(2b) intermediate general qualification": "upper_secondary",
+    "(2c_gen) general maturity certificate": "upper_secondary",
+    "(2c_voc) vocational maturity certificate": "upper_secondary",
+    "(3a) lower tertiary education": "tertiary",
+    "(3b) higher tertiary education": "tertiary",
+}
--- src/soep_cleaning/create_variables/helper.py
+++ src/soep_cleaning/create_variables/helper.py
@@ -1,11 +1,81 @@
 import re
 
-import numpy as np
+from soep_cleaning.config import pd
 
 
-def create_dummies_for_occupation_and_employment_status(out):
-    out["retired"] = out["occupation_status"].dropna() == "NE: Rentner/Rentnerin"
-    out["in_education"] = out["employment_status"].dropna() == "Ausbildung, Lehre"
+def _fail_if_series_wrong_dtype(sr: pd.Series, expected_dtype: str):
+    if expected_dtype not in sr.dtype.name:
+        msg = f"Expected dtype {expected_dtype}, got {sr.dtype.name}"
+        raise TypeError(msg)
+
+
+def _fail_series_without_categories(sr: pd.Series):
+    if sr.cat.categories is None:
+        msg = "Series has no categories"
+        raise ValueError(msg)
+
+
+def _fail_if_invalid_input(input_, expected_dtype: str):
+    if expected_dtype not in str(type(input_)):
+        msg = f"Expected {input_} to be of type {expected_dtype}, got {type(input_)}"
+        raise TypeError(
+            msg,
+        )
+
+
+def _fail_if_invalid_inputs(input_, expected_dtypes: str):
+    if " | " in expected_dtypes:
+        if not any(
+            expected_dtype in str(type(input_))
+            for expected_dtype in expected_dtypes.split(" | ")
+        ):
+            msg = (
+                f"Expected {input_} to be of type {expected_dtypes}, got {type(input_)}"
+            )
+            raise TypeError(
+                msg,
+            )
+
+    else:
+        _fail_if_invalid_input(input_, expected_dtypes)
+
+
+def _error_handling_inputs(
+    input_expected_types: list[list] | None = None,
+):
+    if input_expected_types is None:
+        input_expected_types = [[]]
+    [_fail_if_invalid_inputs(*item) for item in input_expected_types]
+
+
+def create_dummy(
+    sr: pd.Series,
+    true_value: str | list,
+    kind: str = "equality",
+) -> pd.Series:
+    _error_handling_inputs(
+        [
+            [sr, "pandas.core.series.Series"],
+            [true_value, "str | list"],
+            [kind, "str"],
+        ],
+    )
+    if kind == "equality":
+        bool_sr = sr.dropna() == true_value
+    elif kind == "neq":
+        bool_sr = sr.dropna() != true_value
+    elif kind == "isin":
+        bool_sr = sr.dropna().isin(true_value)
+    else:
+        msg = f"Unknown kind '{kind}' of dummy creation"
+        raise ValueError(msg)
+    return bool_sr.astype("bool[pyarrow]")
+
+
+def create_in_education_dummy_categorical(
+    employment: pd.Series,
+    occupation: pd.Series,
+) -> pd.Series:
     in_education = [
         "in Ausbildung, inkl. Weiterbildung, Berufsausbildung, Lehre",
         "in Ausbildung,             inkl. Weiterbildung, Berufsausbildung, Lehre",
@@ -17,35 +87,30 @@ def create_dummies_for_occupation_and_employment_status(out):
         "Aspiranten",
         "NE: in Ausbildung, inkl. Weiterbildung, Berufsausbildung, Lehre",
     ]
-    # Don't use employment status to determine in ausbildung if not working
-    out.loc[out["employment_status"] == "Nicht erwerbstätig", "in_education"] = np.nan
-    out["in_education"] = out["in_education"].fillna(
-        out["occupation_status"].dropna().isin(in_education),
-    )
-    list_of_occ_names = list(out["occupation_status"].dropna().unique())
+    out = create_dummy(employment, "Ausbildung, Lehre")
+    # Don't use employment status to determine in_education if not working
+    out.loc[employment == "Nicht erwerbstätig"] = pd.NA
+    return out.fillna(create_dummy(occupation, in_education, "isin"))
+
+
+def create_selfemployed_occupations(occupation: pd.Series) -> list:
+    list_of_occ_names = list(occupation.dropna().unique())
     occs_of_interest = re.compile("^.*Freiberufler.*$|^.*selbstä.*$")
-    self_employed = list(filter(occs_of_interest.match, list_of_occ_names))
-    out["self_employed"] = out["occupation_status"].dropna().isin(self_employed)
-    out["military"] = out["occupation_status"].dropna() == "NE: Wehr- und Zivildienst"
+    return list(filter(occs_of_interest.match, list_of_occ_names))
 
-    out["erwerbstätig"] = (
-        out["employment_status"].dropna() != "Nicht erwerbstätig"
-    ) & (~out["in_education"].dropna())
-    out["nicht_erwerbstätig"] = (
-        out["employment_status"].dropna() == "Nicht erwerbstätig"
-    )
-    out["unemployed"] = out["occupation_status"].dropna() == "NE: arbeitslos gemeldet"
-    out["full_time"] = out["employment_status"].dropna() == "Voll erwerbstätig"
-    out["part_time"] = out["employment_status"].dropna() == "Teilzeitbeschäftigung"
-    out["geringfügig_erwb"] = (
-        out["employment_status"].dropna() == "Unregelmässig, geringfügig erwerbstät."
-    )
 
-    out["werkstatt"] = (
-        out["employment_status"].dropna() == "Werkstatt für behinderte Menschen"
-    )
-    out["beamte"] = out["occupation_status"].str.startswith("Beamte", na=False)
-    out["parental_leave"] = (
-        out["laborf_status"].dropna() == "NE: Mutterschutz/Elternzeit (seit 1991) "
-    )
-    return out
+def generate_education_variable(casmin: pd.Series, isced: pd.Series, mappings: dict):
+    # Generate qualification variables from casmin and isced education variables
+    # use casmin in education_mapping to create education
+    out = pd.DataFrame()
+    out["education"] = casmin.dropna().map(mappings.casmin)
+
+    # fill missing values from isced in education_mapping
+    out.loc[out["education"].isnull(), "education"] = isced.dropna().map(mappings.isced)
+    val_quali = [
+        "primary_and_lower_secondary",
+        "upper_secondary",
+        "tertiary",
+    ]
+    cat_type = pd.CategoricalDtype(categories=val_quali, ordered=True)
+    return out["education"].astype(cat_type)
