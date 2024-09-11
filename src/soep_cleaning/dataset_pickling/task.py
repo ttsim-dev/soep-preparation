ee4ffd89ff93d8f94cad9cac10ce823b3fdd8129
felixschmitz <s87fschm@uni-bonn.de>
2024-09-11 21:35:01 +0200

    Refactor regex to access the list_of_columns when loading a dataset.

--- src/soep_cleaning/dataset_pickling/task.py
+++ src/soep_cleaning/dataset_pickling/task.py
@@ -1,8 +1,12 @@
+import inspect
+import re
+from importlib.machinery import SourceFileLoader
 from pathlib import Path
 from typing import Annotated
 
 from pytask import task
 from soep_cleaning.config import DATASETS, SOEP_VERSION, SRC, data_catalog, pd
+from soep_cleaning.utilities import dataset_script_name
 
 
 def _iteratively_read_one_dataset(
@@ -37,15 +41,44 @@ def _iteratively_read_one_dataset(
     return out.astype(cat_dtypes)
 
 
+def _list_functions_in_script(script_path: Path) -> list:
+    module = SourceFileLoader(
+        script_path.resolve().stem,
+        str(script_path.resolve()),
+    ).load_module()
+    return [
+        function
+        for function in dir(module)
+        if ("_" not in function) and ("pd" not in function)
+    ]
+
+
+def _list_columns_from_function(script_path: Path, function_name: str) -> list:
+    module = SourceFileLoader(
+        script_path.resolve().stem,
+        str(script_path.resolve()),
+    ).load_module()
+    function_content = inspect.getsource(getattr(module, f"{function_name}"))
+
+    pattern = r'raw\["([a-zA-Z0-9_]+)"\]'
+    return re.findall(pattern, function_content)
+
+
+def _list_of_columns_for_dataset(dataset_name: str) -> list:
+    script_name = f"{dataset_script_name(dataset_name)}_cleaner.py"
+    script_path = SRC.joinpath("initial_cleaning", script_name).resolve()
+    for function in _list_functions_in_script(script_path):
+        if function == dataset_name:
+            return _list_columns_from_function(script_path, function)
+    msg = f"No function found for dataset {dataset_name}."
+    raise ValueError(msg)
+
+
 for dataset in DATASETS:
 
     @task(id=dataset)
     def task_pickle_one_dataset(
         orig_data: Annotated[Path, SRC.joinpath(f"data/{SOEP_VERSION}/{dataset}.dta")],
-        relevant_soep_columns: Annotated[
-            Path,
-            data_catalog["infos"]["relevant_soep_columns"],
-        ],
         dataset: str = dataset,
     ) -> Annotated[pd.DataFrame, data_catalog["raw"][dataset]]:
         """Saves the raw dataset to the data catalog in a more efficient procedure.
@@ -58,8 +91,7 @@ for dataset in DATASETS:
             pd.DataFrame: A pandas DataFrame to be saved to the data catalog.
 
         """
-        columns = pd.read_csv(relevant_soep_columns)[dataset]
-        list_of_columns = columns[columns.notna()].to_list()
+        list_of_columns = _list_of_columns_for_dataset(dataset)
         with pd.read_stata(
             orig_data,
             chunksize=100_000,
