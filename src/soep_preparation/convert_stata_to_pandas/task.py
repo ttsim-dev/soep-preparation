12b2d90168b4abe0328834362cc23e73fc5d00a9
felixschmitz <s87fschm@uni-bonn.de>
2025-04-04 15:04:40 +0200

    Refactor reading STATA files into pytask DataCatalog.

--- src/soep_preparation/convert_stata_to_pandas/task.py
+++ src/soep_preparation/convert_stata_to_pandas/task.py
@@ -7,7 +7,6 @@ from pathlib import Path
 from typing import Annotated
 
 import pandas as pd
-from pandas.api.types import union_categoricals
 from pandas.io.stata import StataReader
 from pytask import task
 
@@ -47,91 +46,29 @@ def _get_relevant_column_names(script: Path) -> list[str]:
     return list(dict.fromkeys(matches))
 
 
-def _extract_num_from_string(s: str):
-    match = re.search(r"\[(-?\d+)\]", s)
-    return int(match.group(1)) if match else float("inf")
-
-
-def _create_categorical_column(out, chunk, col):
-    # If the categories are not of the same type, we convert them to the same type.
-    if out[col].cat.categories.dtype != chunk[col].cat.categories.dtype:
-        # Converting the column's categories dtype
-        # in the chunk to object (the same as in out).
-        if out[col].cat.categories.dtype.name == "object":
-            chunk[col] = chunk[col].cat.set_categories(
-                chunk[col].cat.categories.astype("object"),
-            )
-        else:
-            # Converting the column's categories dtype
-            # of out to the same as of the chunk.
-            out[col] = out[col].cat.set_categories(
-                out[col].cat.categories.astype(
-                    chunk[col].cat.categories.dtype,
-                ),
-            )
-    # Union of the categories of the two columns.
-    orig_categories_union = union_categoricals(
-        [out[col], chunk[col]],
-        ignore_order=True,
-    ).categories.to_list()
-    orig_numeric_categories = sorted(
-        [x for x in orig_categories_union if isinstance(x, int | float)],
-    )
-    orig_string_categories = sorted(
-        [x for x in orig_categories_union if isinstance(x, str)],
-        key=_extract_num_from_string,
-    )
-    # First the string categories, afterwards the numerical.
-    sorted_categories = orig_string_categories + orig_numeric_categories
-    return (
-        out[col].cat.set_categories(
-            sorted_categories,
-            ordered=True,
-        ),
-        chunk[col].cat.set_categories(
-            sorted_categories,
-            ordered=True,
-        ),
-    )
-
-
 def _iteratively_read_one_dataset(
     iterator: StataReader,
     relevant_columns: list[str],
 ) -> pd.DataFrame:
-    out = pd.DataFrame()
-    value_labels = {
+    categorical_mapping = {
         k: v for k, v in iterator.value_labels().items() if k in relevant_columns
     }
+    processed_chunks = []
+
     for chunk in iterator:
-        value_chunk = chunk.replace(value_labels)
-        raw_chunk = value_chunk.astype("category")
-        if out.empty:
-            out = raw_chunk
-        else:
-            col_dtypes = raw_chunk.dtypes
-            for column in relevant_columns:
-                out_chunk = pd.DataFrame(index=raw_chunk.index)
-                if col_dtypes[column].name == "category":
-                    out[column], out_chunk[column] = _create_categorical_column(
-                        out,
-                        raw_chunk,
-                        column,
-                    )
-                else:
-                    out_chunk[column] = chunk[column]
-            out = pd.concat([out, chunk])
-    return out
+        chunk_with_categorical_values = chunk.replace(categorical_mapping)
+        processed_chunks.append(chunk_with_categorical_values)
+    return pd.concat(processed_chunks)
 
 
 for name, catalog in DATA_CATALOGS["single_variables"].items():
 
     @task(id=name)
-    def task_pickle_one_dataset(
+    def task_read_one_dataset(
         stata_data: Annotated[Path, DATA / f"{SOEP_VERSION}" / f"{name}.dta"],
         cleaning_script: Annotated[
             Path,
-            SRC / "initial_cleaning" / f"{name}.py",
+            SRC / "clean_existing_variables" / f"{name}.py",
         ],
     ) -> Annotated[pd.DataFrame, catalog["raw"]]:
         """Saves the raw dataset to the data catalog.
