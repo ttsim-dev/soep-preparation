87dea0c3cc3ee1def5c15258198ec9afe72b80ce
felixschmitz <s87fschm@uni-bonn.de>
2025-06-16 15:17:32 +0200

    Raise no data files found in collection of task, fixing tests.

--- src/soep_preparation/convert_stata_to_pandas/task.py
+++ src/soep_preparation/convert_stata_to_pandas/task.py
@@ -51,43 +51,47 @@ def _iteratively_read_one_file(
     return pd.concat(processed_chunks)
 
 
-if len(DATA_CATALOGS["data_files"]) == 0:
-    msg = """Please add at least one raw file to the data directory under
-    the specified SOEP version and create a cleaning script for it.
-    For further instructions, please refer to the README file."""
-    raise FileNotFoundError(msg)
+if DATA_CATALOGS["data_files"]:
+    for name, catalog in DATA_CATALOGS["data_files"].items():
 
-for name, catalog in DATA_CATALOGS["data_files"].items():
+        @task(id=name)
+        def task_read_one_file(
+            stata_data: Annotated[Path, DATA / f"{SOEP_VERSION}" / f"{name}.dta"],
+            cleaning_script: Annotated[
+                Path,
+                SRC / "clean_existing_variables" / f"{name}.py",
+            ],
+        ) -> Annotated[pd.DataFrame, catalog["raw"]]:
+            """Saves the raw file to the data catalog.
 
-    @task(id=name)
-    def task_read_one_file(
-        stata_data: Annotated[Path, DATA / f"{SOEP_VERSION}" / f"{name}.dta"],
-        cleaning_script: Annotated[
-            Path,
-            SRC / "clean_existing_variables" / f"{name}.py",
-        ],
-    ) -> Annotated[pd.DataFrame, catalog["raw"]]:
-        """Saves the raw file to the data catalog.
+            Parameters:
+                stata_data: The path to the original STATA file.
+                cleaning_script: The path to the respective cleaning script.
 
-        Parameters:
-            stata_data: The path to the original STATA file.
-            cleaning_script: The path to the respective cleaning script.
+            Returns:
+                    The raw data to be saved to the data catalog.
 
-        Returns:
-                The raw data to be saved to the data catalog.
+            Raises:
+                TypeError: If input data or script path is not of expected type.
+            """
+            _error_handling_task(stata_data, cleaning_script)
+            relevant_columns = _get_relevant_column_names(cleaning_script)
+            with StataReader(
+                stata_data,
+                chunksize=100_000,
+                columns=relevant_columns,
+                convert_categoricals=False,
+            ) as stata_iterator:
+                return _iteratively_read_one_file(stata_iterator, relevant_columns)
 
-        Raises:
-            TypeError: If input data or script path is not of expected type.
-        """
-        _error_handling_task(stata_data, cleaning_script)
-        relevant_columns = _get_relevant_column_names(cleaning_script)
-        with StataReader(
-            stata_data,
-            chunksize=100_000,
-            columns=relevant_columns,
-            convert_categoricals=False,
-        ) as stata_iterator:
-            return _iteratively_read_one_file(stata_iterator, relevant_columns)
+else:
+
+    @task
+    def _raise_data_files_found() -> None:
+        msg = """Please add at least one raw file to the data directory under
+        the specified SOEP version and create a cleaning script for it.
+        For further instructions, please refer to the README file."""
+        raise FileNotFoundError(msg)
 
 
 def _error_handling_task(data, script_path):
