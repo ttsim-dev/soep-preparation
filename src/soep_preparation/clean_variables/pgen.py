4616285271b680379beed0a8390112b8cbbceb0b
felixschmitz <s87fschm@uni-bonn.de>
2025-07-21 11:48:46 +0200

    Move month mapping to utilities.

--- src/soep_preparation/clean_variables/pgen.py
+++ src/soep_preparation/clean_variables/pgen.py
@@ -4,7 +4,7 @@ import re
 
 import pandas as pd
 
-from soep_preparation.clean_variables import month_mapping
+from soep_preparation.utilities import month_mapping
 from soep_preparation.utilities.data_manipulator import (
     apply_smallest_int_dtype,
     create_dummy,
